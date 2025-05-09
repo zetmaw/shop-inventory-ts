65a0b00759edd2e4a91a04989eab5c9b38e7dfa6
Willie Neumann <musicman@Willies-iMac.local>
2025-05-09 10:36:13 -0400

    Switch Vite config to JS to satisfy Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-// ESM‑friendly __dirname
+// __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname  = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
